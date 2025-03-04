ec8fb750c26bad226bb0c2f86a7658895eefbf1b
Satyam216 <satyamjain216@gmail.com>
2025-03-05 00:26:03 +0530

    Room Creation

--- models/Room.js
+++ models/Room.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const RoomSchema = new mongoose.Schema({
     roomId: { type: String, unique: true, required: true },
-    users: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }], // Store user IDs
+    users: [{ type: String, ref: "User" }], // Store user IDs
     createdAt: { type: Date, default: Date.now },
 });
 
